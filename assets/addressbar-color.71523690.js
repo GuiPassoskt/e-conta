b0a8f315e617506c630337a312102d063684fac7
Guilherme Passos <guipassoskt@gmail.com>
2023-10-03 21:46:32 -0300

    Substituição pelo spa

--- assets/addressbar-color.71523690.js
+++
@@ -1,1 +0,0 @@
-import{b as o,A as s}from"./index.ff6764e0.js";const a=s.set("#021354");var e=o(({app:r})=>{r.config.globalProperties.$addressbarColor=a});export{a as addressbarColor,e as default};
---
+++ assets/addressbar-color.fea7dedc.js
@@ -0,0 +1,1 @@
+import{b as o,A as s}from"./index.e8e10fc1.js";const a=s.set("#021354");var e=o(({app:r})=>{r.config.globalProperties.$addressbarColor=a});export{a as addressbarColor,e as default};
